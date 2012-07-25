3904aec793d86a8564b6b92aab76a85698fd6821
George Hahn <george.hahn.vhs@gmail.com>
2012-07-24 21:35:11 -0400

    Light refactoring, added NUnit, wrote a test

---
+++ EESpeak/EESpeak.Tests/UnitConverterTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using EESpeak;
+
+namespace EESpeak.Tests
+{
+    [TestFixture]
+    public class UnitConverterTest
+    {
+        [Test]
+        public void ConvertToEngineeringNotation()
+        {
+            Tuple<double, string>[] values =
+            {
+                new Tuple<double, string>(5.10, "5.1"),
+                new Tuple<double, string>(510, "510"),
+                new Tuple<double, string>(5100, "5.1k"),
+                new Tuple<double, string>(51000, "51k"),
+                new Tuple<double, string>(1e6, "1M"),
+                new Tuple<double, string>(1e8, "100M"),
+                new Tuple<double, string>(1e9, "1G"),
+                new Tuple<double, string>(1e12, "1T"),
+                new Tuple<double, string>(1e15, "1P"),
+                new Tuple<double, string>(1e18, "1E"),
+                new Tuple<double, string>(1e21, "1Z"),
+                new Tuple<double, string>(1e24, "1Y")
+            };
+
+            foreach(Tuple<double, string> value in values)
+                Assert.AreEqual(value.Item2, UnitConverter.ToEngineeringNotation(value.Item1));
+            
+        }
+    }
+}
--- EESpeak/EESpeak/MainForm.cs
+++ EESpeak/EESpeak/MainForm.cs
@@ -140,7 +140,7 @@ namespace EESpeak
             if (Matches(e.Result.Text, fourBandStrings))
             {
                 // Determine the unit
-                string unit = GetUnit(e.Result.Text);
+                string unit = UnitConverter.GetUnit(e.Result.Text);
 
                 // Evaluate the four bands.
                 string[] result = ParseBands(unit,
@@ -162,7 +162,7 @@ namespace EESpeak
             else if (Matches(e.Result.Text, fiveBandStrings))
             {
                 // Determine the unit
-                string unit = GetUnit(e.Result.Text);
+                string unit = UnitConverter.GetUnit(e.Result.Text);
 
                 // Evaluate the four bands.
                 string[] result = ParseBands(unit,
@@ -184,7 +184,7 @@ namespace EESpeak
             else if (Matches(e.Result.Text, eiaCommandStrings))
             {
                 // Determine the unit
-                string unit = GetUnit(e.Result.Text);
+                string unit = UnitConverter.GetUnit(e.Result.Text);
 
                 // Evaluate
                 string[] result = ParseEIA(unit, (int)e.Result.Semantics["first_number"].Value,
@@ -395,7 +395,11 @@ namespace EESpeak
 
             Renard *= Math.Pow(10, Multiplier);
 
-            string[] result = { ToEngineeringNotation(Renard) + unit, Renard + "," + GetSpokenUnit(unit), ToEngineeringNotationSpeech(Renard) + "-" + GetSpokenUnit(unit) };
+            string[] result = { 
+                UnitConverter.ToEngineeringNotation(Renard) + unit, 
+                Renard + "," + UnitConverter.GetSpokenUnit(unit),
+                UnitConverter.ToEngineeringNotationSpeech(Renard) + "-" + 
+                    UnitConverter.GetSpokenUnit(unit) };
 
             return result;
         }
@@ -442,7 +446,7 @@ namespace EESpeak
                 tolerance += GetTolerance(fourth) + "%";
             }
 
-            string[] result = { ToEngineeringNotation(retValue) + unit + tolerance, retValue + GetSpokenUnit(unit) + "," + tolerance, ToEngineeringNotationSpeech(retValue) + "-" + GetSpokenUnit(unit) + "," + tolerance };
+            string[] result = { UnitConverter.ToEngineeringNotation(retValue) + unit + tolerance, retValue + UnitConverter.GetSpokenUnit(unit) + "," + tolerance, UnitConverter.ToEngineeringNotationSpeech(retValue) + "-" + UnitConverter.GetSpokenUnit(unit) + "," + tolerance };
 
             return result;
         }
@@ -473,46 +477,6 @@ namespace EESpeak
             throw new KeyNotFoundException();
         }
 
-        private string GetUnit(string command)
-        {
-            if (command.Contains("capacitor"))
-            {
-                return "F";
-            }
-
-            if (command.Contains("resistor"))
-            {
-                return "Ω";
-            }
-
-            if (command.Contains("inductor"))
-            {
-                return "H";
-            }
-
-            return "";
-        }
-
-        private string GetSpokenUnit(string unit)
-        {
-            if (unit == "F")
-            {
-                return "Farad";
-            }
-
-            if (unit == "Ω")
-            {
-                return "Ohm";
-            }
-
-            if (unit == "H")
-            {
-                return "Henry";
-            }
-
-            return "";
-        }
-
         private bool Matches(string command, string[] phrases)
         {
             foreach (string s in phrases)
@@ -525,175 +489,6 @@ namespace EESpeak
             return false;
         }
 
-        private string ToEngineeringNotation(double d)
-        {
-            double exp = Math.Log10(Math.Abs(d));
-            if (Math.Abs(d) >= 1)
-            {
-                switch ((int)Math.Floor(exp))
-                {
-                    case 0:
-                    case 1:
-                    case 2:
-                        return d.ToString();
-                    case 3:
-                    case 4:
-                    case 5:
-                        return (d / 1e3).ToString() + "k";
-                    case 6:
-                    case 7:
-                    case 8:
-                        return (d / 1e6).ToString() + "M";
-                    case 9:
-                    case 10:
-                    case 11:
-                        return (d / 1e9).ToString() + "G";
-                    case 12:
-                    case 13:
-                    case 14:
-                        return (d / 1e12).ToString() + "T";
-                    case 15:
-                    case 16:
-                    case 17:
-                        return (d / 1e15).ToString() + "P";
-                    case 18:
-                    case 19:
-                    case 20:
-                        return (d / 1e18).ToString() + "E";
-                    case 21:
-                    case 22:
-                    case 23:
-                        return (d / 1e21).ToString() + "Z";
-                    default:
-                        return (d / 1e24).ToString() + "Y";
-                }
-            }
-            else if (Math.Abs(d) > 0)
-            {
-                switch ((int)Math.Floor(exp))
-                {
-                    case -1:
-                    case -2:
-                    case -3:
-                        return (d * 1e3).ToString() + "m";
-                    case -4:
-                    case -5:
-                    case -6:
-                        return (d * 1e6).ToString() + "μ";
-                    case -7:
-                    case -8:
-                    case -9:
-                        return (d * 1e9).ToString() + "n";
-                    case -10:
-                    case -11:
-                    case -12:
-                        return (d * 1e12).ToString() + "p";
-                    case -13:
-                    case -14:
-                    case -15:
-                        return (d * 1e15).ToString() + "f";
-                    case -16:
-                    case -17:
-                    case -18:
-                        return (d * 1e15).ToString() + "a";
-                    case -19:
-                    case -20:
-                    case -21:
-                        return (d * 1e15).ToString() + "z";
-                    default:
-                        return (d * 1e15).ToString() + "y";
-                }
-            }
-            else
-            {
-                return "0";
-            }
-        }
-
-        private string ToEngineeringNotationSpeech(double d)
-        {
-            double exp = Math.Log10(Math.Abs(d));
-            if (Math.Abs(d) >= 1)
-            {
-                switch ((int)Math.Floor(exp))
-                {
-                    case 0:
-                    case 1:
-                    case 2:
-                        return d.ToString();
-                    case 3:
-                    case 4:
-                    case 5:
-                        return (d / 1e3).ToString() + "kilo";
-                    case 6:
-                    case 7:
-                    case 8:
-                        return (d / 1e6).ToString() + "Mega";
-                    case 9:
-                    case 10:
-                    case 11:
-                        return (d / 1e9).ToString() + "Giga";
-                    case 12:
-                    case 13:
-                    case 14:
-                        return (d / 1e12).ToString() + "Tera";
-                    case 15:
-                    case 16:
-                    case 17:
-                        return (d / 1e15).ToString() + "Peta";
-                    case 18:
-                    case 19:
-                    case 20:
-                        return (d / 1e18).ToString() + "Exa";
-                    case 21:
-                    case 22:
-                    case 23:
-                        return (d / 1e21).ToString() + "Z";
-                    default:
-                        return (d / 1e24).ToString() + "Y";
-                }
-            }
-            else if (Math.Abs(d) > 0)
-            {
-                switch ((int)Math.Floor(exp))
-                {
-                    case -1:
-                    case -2:
-                    case -3:
-                        return (d * 1e3).ToString() + "milli";
-                    case -4:
-                    case -5:
-                    case -6:
-                        return (d * 1e6).ToString() + "micro";
-                    case -7:
-                    case -8:
-                    case -9:
-                        return (d * 1e9).ToString() + "nano";
-                    case -10:
-                    case -11:
-                    case -12:
-                        return (d * 1e12).ToString() + "pico";
-                    case -13:
-                    case -14:
-                    case -15:
-                        return (d * 1e15).ToString() + "femto";
-                    case -16:
-                    case -17:
-                    case -18:
-                        return (d * 1e15).ToString() + "atto";
-                    case -19:
-                    case -20:
-                    case -21:
-                        return (d * 1e15).ToString() + "z";
-                    default:
-                        return (d * 1e15).ToString() + "y";
-                }
-            }
-            else
-            {
-                return "0";
-            }
-        }
 
         private void eESpeakHomepageToolStripMenuItem_Click(object sender, EventArgs e)
         {
